9b80fde5f29fc78d60d3ec9822bd869037d76b89
Augusto Mota <augustomota180798@gmail.com>
2022-12-29 03:48:19 -0300

    lista mercado

---
+++ supermercado.js
@@ -0,0 +1,50 @@
+var items = []
+
+
+document.querySelector('input[type=submit]')
+.addEventListener('click',()=>{
+    var nomeProduto = document.querySelector('input[name=nome_produto]').value;
+    var precoProduto = document.querySelector('input[name=valor_produto]').value;
+
+
+    items.push({
+        nome: nomeProduto,
+        valor: precoProduto
+    });
+
+    /*
+        <div class="lista-produto-single">
+                    <h3>Red Bull</h3>
+                    <h3 class="price-produto"><span>$ 20,00</span></h3>
+        </div>
+    */
+
+    let listaProdutos = document.querySelector('.lista-produtos');
+    let soma = 0;
+    listaProdutos.innerHTML = "";
+    items.map(function(val){
+        soma+=parseFloat(val.valor);
+        listaProdutos.innerHTML+=`
+        <div class="lista-produto-single">
+                <h3>`+val.nome+`</h3>
+                <h3 class="price-produto"><span>R$`+val.valor+`</span></h3>
+        </div>
+        `;
+    })
+    soma = soma.toFixed(2);
+    nomeProduto.value = "";
+    precoProduto.value = "";
+
+    let elementoSoma = document.querySelector('.soma-produto h1');
+    elementoSoma.innerHTML = 'R$'+soma;
+
+
+});
+
+document.querySelector('button[name=limpar]')
+.addEventListener('click',()=>{
+   items = [];
+
+   document.querySelector('.lista-produtos').innerHTML = "";
+   document.querySelector('.soma-produto h1').innerHTML = "R$0";
+});
